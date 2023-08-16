44e1a445071691f46be1b589db04ffa7588ee226
Lucas Langeloh <lucas.l.scipioni@gmail.com>
2023-08-16 11:19:27 -0300

    Fix to BG

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.sass'
 import GameCard from './components/gameCard'
+import BG from './assets/bg.jpg';
 
 function App() {
   return (
     <>
-      <div className="streaky-shooter">
+      <div className="streaky-shooter" style={{background: `url(${BG}`, backgroundSize: 'cover'}} >
         <div className='streaky-shooter__sidebar'>
           <span>The Streaky Shooter</span>
         </div>
